32b57e0e2e10d74b7118f731beb9e5cb17690aab
Lee Wei Heng <leeweiheng@WHs-MacBook-Pro.local>
2020-09-20 14:28:22 +0800

    Add checkstyle fixes

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -92,14 +92,13 @@ public class Person {
         return otherPerson.getName().equals(getName())
                 && otherPerson.getPhone().equals(getPhone())
                 && otherPerson.getEmail().equals(getEmail())
-                && otherPerson.getTags().equals(getTags())
-                && otherPerson.getRemark().equals(getRemark());
+                && otherPerson.getTags().equals(getTags());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, tags, remark);
+        return Objects.hash(name, phone, email, tags);
     }
 
     @Override
